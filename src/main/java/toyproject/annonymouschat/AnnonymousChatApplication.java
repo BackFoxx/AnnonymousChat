47520a321f3f81a9e2804ba206dc5f7990831d8e
조승현 <jorealbusiness@gmail.com>
2022-04-23 16:55:24 +0900

    프로젝트 생성

---
+++ src/main/java/toyproject/annonymouschat/AnnonymousChatApplication.java
@@ -0,0 +1,13 @@
+package toyproject.annonymouschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnonymousChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnonymousChatApplication.class, args);
+	}
+
+}
