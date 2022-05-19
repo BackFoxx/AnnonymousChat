7ff8ab2d43a8abf80e7a2aeeaf4a2589d372ef5e
조승현 <jorealbusiness@gmail.com>
2022-05-19 22:14:02 +0900

    V 0.1.7 (변경 내용은 노션에 정리)

--- src/main/java/toyproject/annonymouschat/User/controller/UserRegistrationServlet.java
+++ src/main/java/toyproject/annonymouschat/User/controller/UserRegistrationServlet.java
@@ -2,37 +2,52 @@ package toyproject.annonymouschat.User.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import toyproject.annonymouschat.User.dto.UserRegistrationDto;
+import toyproject.annonymouschat.User.dto.UserResponseDto;
 import toyproject.annonymouschat.User.service.UserService;
-import toyproject.annonymouschat.config.controller.controller.ControllerWithMap;
-import toyproject.annonymouschat.config.controller.ModelView;
-import toyproject.annonymouschat.config.controller.ReturnType;
+import toyproject.annonymouschat.config.exception.WrongFormException;
+import toyproject.annonymouschat.config.controller.customAnnotation.MyRequestBody;
+import toyproject.annonymouschat.config.controller.controller.ControllerAutoJson;
+import toyproject.annonymouschat.config.controller.customAnnotation.ReturnType;
+import toyproject.annonymouschat.config.controller.customAnnotation.MyHttpResponse;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Map;
+import java.util.regex.Pattern;
 
 @Slf4j
-//@WebServlet(name = "registration", urlPatterns = "/login/registration")
-public class UserRegistrationServlet implements ControllerWithMap {
+public class UserRegistrationServlet implements ControllerAutoJson<UserRegistrationDto> {
     private UserService userService = new UserService();
+    @MyHttpResponse
+    HttpServletResponse response;
 
     @Override
-    @ReturnType(type = ReturnType.ReturnTypes.REDIRECT)
-    public ModelView process(Map<String, Object> requestParameters) {
-        String userEmail = (String) requestParameters.get("userEmail");
-        String password = (String) requestParameters.get("password");
+    @ReturnType(type = ReturnType.ReturnTypes.JSON)
+    public UserResponseDto process(@MyRequestBody UserRegistrationDto registrationDto) {
 
-        UserRegistrationDto registrationDto = new UserRegistrationDto(userEmail, password);
-        String savedEmail = userService.registration(registrationDto);
+        try {
+            if (!Pattern.compile("^(.+)@(.+)$").matcher(registrationDto.getUserEmail()).matches()) {
+                throw new WrongFormException("이메일 형식이 아닙니다.");
+            }
+            if (registrationDto.getUserEmail().isEmpty()) {
+                throw new WrongFormException("이메일 주소를 입력해주세요");
+            }
+            if (!Pattern.compile("^.{4,}").matcher(registrationDto.getPassword()).matches()) {
+                throw new WrongFormException("비밀번호는 4자 이상이어야 합니다");
+            }
+            if (registrationDto.getUserEmail().length() > 20) {
+                throw new WrongFormException("비밀번호는 20자가 넘어가지 않게 해주세요");
+            }
 
-        ModelView modelView = new ModelView("/v/login/login-form");
+            String savedEmail = userService.registration(registrationDto);
 
-        HttpServletResponse httpServletResponse = (HttpServletResponse) requestParameters.get("httpServletResponse");
-        Cookie registerEmailCookie = new Cookie("registerEmail", userEmail);
-        registerEmailCookie.setPath("/v/login/login-form");
-        registerEmailCookie.setMaxAge(1);
-        httpServletResponse.addCookie(registerEmailCookie);
+            Cookie registerEmailCookie = new Cookie("registerEmail", savedEmail);
+            registerEmailCookie.setPath("/v/login/login-form");
+            registerEmailCookie.setMaxAge(5);
+            response.addCookie(registerEmailCookie);
 
-        return modelView;
+            return new UserResponseDto(true, "회원가입 완료되었습니다.");
+        } catch (WrongFormException e) {
+            return new UserResponseDto(false, e.getMessage());
+        }
     }
 }
